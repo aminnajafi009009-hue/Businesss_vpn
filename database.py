002e33fcfdf5a003722b61379b69eb6abe0642a5
aminnajafi009009-hue <aminnajafi009009@gmail.com>
2026-06-30 18:49:56 +0330

    Update database.py

--- database.py
+++ database.py
@@ -134,9 +134,7 @@ def _generate_invite_code() -> str:
             return code
 
 
-from typing import Optional
-
-def get_user(telegram_id) -> Optional[sqlite3.Row]:
+def get_user(telegram_id) -> sqlite3.Row | None:
     cur = get_connection().cursor()
     cur.execute("SELECT * FROM users WHERE telegram_id = ?", (str(telegram_id),))
     return cur.fetchone()
